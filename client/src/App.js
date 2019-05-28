5a2d48818c6f5251accd0f05b1cc7ab7c9873ac7
tariqys995 <tariqys995@gmail.com>
2019-05-27 23:45:17 -0400

    Final version

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import "./App.css";
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//Components
-import Navbar from "./components/layouts/Navbar";
-import Landing from "./components/layouts/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layouts/Alert";
-import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Post from "./components/post/Post";
-import Posts from "./components/posts/Posts";
-//Redux
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Routes from "./components/routing/Routes";
+
+// Redux
 import { Provider } from "react-redux";
-import store from "./store/store";
-import setAuthToken from "./utils/setAuthToken";
+import store from "./store";
 import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 //we run it here aswell because in our actions it only runs the first time the user load, we want it to run everytime
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -32,46 +19,16 @@ const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Router />
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/profile/:id' component={Profile} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/posts/:id' component={Post} />
-              <PrivateRoute />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
